344dc3b8ec3cbd83c98d5932c218afc7abf12b76
Watanabe, Mitsutoshi <mitsutoshi@reivo.co.jp>
2014-03-14 15:00:50 +0900

    accordable-ul got acceptable the css properties change on open/close event.

--- accordable-ul.js
+++ accordable-ul.js
@@ -1,4 +1,5 @@
-var accordable_ul = function(/* jquery_object=*/ $$) {
+var accordable_ul = function(/* jquery_object=*/ $$, /* css=*/ css) {
+
   var init = function() {
     $li = $$.find('li');
     // Set object style
@@ -8,10 +9,34 @@ var accordable_ul = function(/* jquery_object=*/ $$) {
     $li.click(function(){
       $child_ul = $(this).children('ul');
       if ( $child_ul.is(':visible') ) { // close
+        // css change
+        if ( typeof css != 'undefined' && typeof css.close != 'undefined' ) {
+          for ( var selector_str in css.close ) {
+            var  $selector = $(this);
+            if ( selector_str != ':self' ) {
+              $selector = $(this).find(selector_str);
+            }
+            $selector.css(css.close[selector_str]);
+          }
+        }
+
+        // animation
         $child_ul.animate({height: 'hide'}, 100, 'swing', function(){
           $(this).hide();
         });
       } else { // open
+        // css change
+        if ( typeof css != 'undefined' && typeof css.close != 'undefined' ) {
+          for ( var selector_str in css.open ) {
+            var $selector = $(this);
+            if ( selector_str != ':self' ) {
+              $selector = $(this).find(selector_str);
+            }
+            $selector.css(css.open[selector_str]);
+          }
+        }
+        
+        // animation
         $child_ul.animate({height: 'show'}, 100, 'swing').show();
       }
     });
--- example/js/init.js
+++ example/js/init.js
@@ -1,4 +1,24 @@
 $(function(){
   hover_pulldown($('.hover-pulldown')).init();
-  accordable_ul($('.accordable-ul')).init();
+  accordable_ul(
+    $('.accordable-ul'),
+    {
+      close: {
+        ':self': {
+          background: '#00f'
+        },
+        'span': {
+          color: '#000'
+        }
+      },
+      open: {
+        ':self': {
+          background: '#f00'
+        },
+        'span': {
+          color: '#fff'
+        }
+      }
+    }
+  ).init();
 });
