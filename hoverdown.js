fb0d7a14d26551f616844ecb0742e6d0c1915786
Watanabe, Mitsutoshi <mitsutoshi@reivo.co.jp>
2014-04-04 16:26:00 +0900

    hoverpulldown got preload function.

--- hoverdown.js
+++ hoverdown.js
@@ -1,9 +1,15 @@
-var hover_pulldown = function($$) {
+var hover_pulldown = function($$, options) {
+  var default_options = {
+    is_img_separated: true,
+    is_preload: true,
+  };
+  options = $.extend(true, options, default_options);
+  
   // Initialize the each jquery object
   var init = function() {
     // Set object style
     $$.find('ul').hide();
-    $$.find('ul').css({position: 'absolute'});
+    $$.find('ul').css({position: 'absolute', 'z-index': '9999'});
 
     // Events
     $$.hover(
@@ -23,6 +29,10 @@ var hover_pulldown = function($$) {
         var splitted = $main_button.attr('src').split('.');
         var filename = splitted[0];
         var ext = splitted[1];
+        if ( options.is_preload ) {
+          $('<img>').attr('src', filename + '_bk' + '.' + ext);
+          $('<img>').attr('src', filename + '.' + ext);
+        }
         
         return {
           //filename: filename,
